3e101c7ab124e50517fa78f27024245a6ae8bad6
Isaias Gonçalves <131933508+IsaiasZaza@users.noreply.github.com>
2024-08-01 16:44:18 -0300

    feat: created diferencas Lancamento

--- src/app/components/Lancamento.jsx
+++ src/app/components/Lancamento.jsx
@@ -1,24 +1,31 @@
 "use client";
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import { BsHandbag } from "react-icons/bs";
 
-
 const Lancamento = () => {
+    const [aparecer, setAparecer] = useState(true);
+
+    const toggleCategory = () => {
+        setAparecer(!aparecer);
+    }
+
     return (
         <div className="container mx-auto py-8 px-10">
-            <h2 className="text-center text-2xl font-bold mb-6">LANÇAMENTOS</h2>
+            <h2 onClick={toggleCategory} className="text-center text-3xl font-bold mb-6 cursor-pointer text-red-600 hover:text-red-800 transition-colors">
+                {aparecer ? "ACESSE MAQUIAGENS" : "ACESSE ACESSÓRIOS"}
+            </h2>
             <Swiper
                 spaceBetween={30}
                 navigation={true}
                 pagination={{
                     clickable: true,
                 }}
-                modules={[Navigation]}
+                modules={[Navigation, Pagination]}
                 breakpoints={{
                     640: {
                         slidesPerView: 1,
@@ -35,67 +42,209 @@ const Lancamento = () => {
                 }}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div className="flex flex-col items-center text-center">
-                        <img
-                            src="/batao-bala.jpg"
-                            alt="Product 1"
-                            className="mb-4"
-                        />
-                        <h3 className="text-lg font-medium">Refil Creme Acetinado Desodorante Hidratante Corporal Lily Absolu 250g</h3>
-                        <p className="text-gray-800 font-bold">R$ 75,90</p>
-                        <button className="mt-2 py-2 px-4 rounded-full flex bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all "><BsHandbag className='text-xl mr-1' />Comprar</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center text-center">
-                        <img
-                            src="/limpador.jpg"
-                            alt="Product 1"
-                            className="mb-4"
-                        />
-                        <h3 className="text-lg font-medium">Refil Creme Acetinado Hidratante Desodorante Corporal Lily 250g</h3>
-
-                        <p className="text-gray-800 font-bold">R$ 75,90</p>
-                        <button className="mt-2 py-2 px-4 rounded-full flex bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all"><BsHandbag className='text-xl mr-1' />Comprar</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center text-center">
-                        <img
-                            src="/base-liquida.jpg"
-                            alt="Product 1"
-                            className="mb-4"
-                        />
-                        <h3 className="text-lg font-medium">Gloss Labial Vermelho Morango Intense Bubbaloo 5ml</h3>
-                        <p className="text-gray-800 font-bold">R$ 42,90</p>
-                        <button className="mt-2 py-2 px-4 rounded-full flex bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all"><BsHandbag className='text-xl mr-1' />Comprar</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center text-center">
-                        <img
-                            src="/brinco.jpg"
-                            alt="Product 1"
-                            className="mb-4"
-                        />
-                        <h3 className="text-lg font-medium">Sabonete Líquido Corpo Nativa SPA Ameixa Negra 200ml</h3>
-                        <p className="text-gray-800 font-bold">R$ 49,90</p>
-                        <button className="mt-2 py-2 px-4 rounded-full flex bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all"><BsHandbag className='text-xl mr-1' />Comprar</button>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className="flex flex-col items-center text-center">
-                        <img
-                            src="/bolsa.jpg"
-                            alt="Product 1"
-                            className="mb-4"
-                        />
-                        <h3 className="text-lg font-medium">Sabonete Líquido Corpo Nativa SPA Ameixa Negra 200ml</h3>
-                        <p className="text-gray-800 font-bold">R$ 49,90</p>
-                        <button className="mt-2 py-2 px-4 rounded-full flex bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all"><BsHandbag className='text-xl mr-1' />Comprar</button>
-                    </div>
-                </SwiperSlide>
+                {aparecer ? (
+                    <>
+                        <SwiperSlide>  {/*ACESSORIOSSSSS*/}
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/batao-bala.jpg"
+                                    alt="Product 1"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">a Creme Acetinado Desodorante Hidratante Corporal Lily Absolu 250g</h3>
+                                <p className="text-gray-800 font-bold">R$ 75,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/limpador.jpg"
+                                    alt="Product 2"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Refil Creme Acetinado Hidratante Desodorante Corporal Lily 250g</h3>
+                                <p className="text-gray-800 font-bold">R$ 75,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Product 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Gloss Labial Vermelho Morango Intense Bubbaloo 5ml</h3>
+                                <p className="text-gray-800 font-bold">R$ 42,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                    </>
+                ) : (
+                    <>
+                        <SwiperSlide> {/*MAQUIAGENS*/}
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 1"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Brinco de Argola Grande</h3>
+                                <p className="text-gray-800 font-bold">R$ 39,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 2"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Bolsa de Couro Preta</h3>
+                                <p className="text-gray-800 font-bold">R$ 149,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="flex flex-col items-center text-center">
+                                <img
+                                    src="/base-liquida.jpg"
+                                    alt="Accessory 3"
+                                    className="mb-4"
+                                />
+                                <h3 className="text-lg font-medium">Óculos de Sol Estiloso</h3>
+                                <p className="text-gray-800 font-bold">R$ 89,90</p>
+                                <button className="mt-2 py-2 px-4 rounded-full flex items-center bg-red-500 text-white font-semibold hover:bg-red-700 hover:text-gray-50 transition-all">
+                                    <BsHandbag className='text-xl mr-1' /> Comprar
+                                </button>
+                            </div>
+                        </SwiperSlide>
+                    </>
+                )}
             </Swiper>
         </div>
     );
